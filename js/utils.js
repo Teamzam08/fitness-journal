9df9598ee785273b68bef59a9fe40a58fe36bcb2
ADRYAN HAWKINS <teamzaal@gmail.com>
2026-01-10 23:11:18 -0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,10 @@
+// =========================
+// App Version
+// =========================
+const APP_VERSION = "v0.1.0-beta";
+window.APP_VERSION = APP_VERSION;
+
+
 function uuid() {
   return crypto.randomUUID();
 }
