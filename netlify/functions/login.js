9b52d9c537a4a3c09200d01f7b311bae1f09f130
ADRYAN HAWKINS <teamzaal@gmail.com>
2026-01-23 17:31:39 -0500

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,1 +1,25 @@
+async function loginUser(username, password, rememberMe = false) {
+  const passwordHash = await hashPassword(password);
+
+  const res = await fetch("/.netlify/functions/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, passwordHash })
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || "Login failed");
+  }
+
+  state.currentUser = data.username;
+  state.users[data.username] = data.data;
+
+  if (rememberMe) {
+    markTrustedDevice(username);
+  }
+
+  saveState(state);
+}
 
