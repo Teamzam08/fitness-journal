485a88393c4984f2dc5341bdc1de787db6b38e83
ADRYAN HAWKINS <teamzaal@gmail.com>
2026-01-23 17:31:18 -0500

    Update register.js

--- netlify/functions/register.js
+++ netlify/functions/register.js
@@ -1,1 +1,20 @@
+async function registerUser(username, password) {
+  const passwordHash = await hashPassword(password);
+
+  const res = await fetch("/.netlify/functions/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, passwordHash })
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error || "Registration failed");
+  }
+
+  state.currentUser = data.username;
+  state.users[data.username] = data.data;
+  saveState(state);
+}
 
